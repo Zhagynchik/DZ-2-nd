0429e3aefbcc6441f2891b906f248b34a52d4ec0
zhagynchik <zhagynchik@mail.ru>
2022-12-03 18:28:57 +0300

    First commit

---
+++ DZ2.2/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6 */
+
+Console.WriteLine($"Введите число: ");
+string numberStr = Console.ReadLine();
+int number = Convert.ToInt32(numberStr);
+
+if (number <= 99)
+{
+     Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    while (number >=1000)
+    {
+    number = number / 10;
+    }
+    number = number % 10;
+    Console.WriteLine(number);
+}
+
+
