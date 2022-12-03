122c47943d03108120ee9fd6c87ff3d34a661e09
zhagynchik <zhagynchik@mail.ru>
2022-12-03 18:31:02 +0300

    First commit

---
+++ DZ2.3/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+6 -> да
+7 -> да
+1 -> нет */
+
+
+Console.WriteLine($"Введите день недели соответствующей дню цифрой от 1 до 7: ");
+string numberStr = Console.ReadLine();
+int number = Convert.ToInt32(numberStr);
+
+if (number > 6) 
+{
+   Console.WriteLine("Выходной день");
+}
+else
+{
+    Console.WriteLine("Будний день");
+}
+
+
+
