31e79c565b577ab172086fe2851e32503774a79d
zhagynchik <zhagynchik@mail.ru>
2022-12-03 18:21:58 +0300

    First commit

---
+++ DZ2.1/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1*/
+
+
+Console.WriteLine($"Введите трехзначное число: ");
+string numberStr = Console.ReadLine();
+int number = Convert.ToInt32(numberStr);
+int secondDigit = number / 10 % 10;
+Console.WriteLine(secondDigit);
+
+
+
